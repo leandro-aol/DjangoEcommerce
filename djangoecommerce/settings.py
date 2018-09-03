dcec930be7138ee4515f422ac891f488ae04a353
Leandro <leandro@live.com.my>
2018-09-02 23:01:28 -0300

    Alterando o link do minha conta para apontar para o Admin. Próximo passo: criar a app Catalog

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -131,3 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+#LOGIN_URL = 'login'
+#LOGIN_REDIRECT_URL = 'core_index'
+LOGOUT_REDIRECT_URL = 'core_index'
