ca01c6b2457984fa9ec5c400d0daf7fc39f38217
Leandro <leandro@live.com.my>
2018-09-04 16:59:44 -0300

    Default DEBUG=False

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', '[default-value]')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
